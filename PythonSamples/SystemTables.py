d8546197d92f7d38c2425e727de9e5a421b1dbb7
zanasrin <zanasrin@microsoft.com>
2017-11-13 11:31:20 +0530

    connect using ssl

--- PythonSamples/SystemTables.py
+++ PythonSamples/SystemTables.py
@@ -3,17 +3,21 @@ from cassandra.auth import PlainTextAuthProvider
 import config as cfg
 from cassandra.query import BatchStatement, SimpleStatement
 from prettytable import PrettyTable
+from ssl import PROTOCOL_TLSv1_2
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
 
 def PrintTable(rows):
     t = PrettyTable(['ID', 'FirstName', 'LastName'])
     for user_row in rows:
         t.add_row([user_row.id, user_row.firstname, user_row.lastname])
     print t
-
-auth_provider = PlainTextAuthProvider(
-        username=cfg.config['username'], password=cfg.config['password'])
-cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_provider=auth_provider)
-session = cluster.connect()
+    
+ssl_opts = {
+    'ca_certs': DEFAULT_CA_BUNDLE_PATH,
+    'ssl_version': PROTOCOL_TLSv1_2,
+}
+if 'selfsigned_cert' in cfg.config:
+    ssl_opts['ca_certs'] = cfg.config['selfsigned_cert']
 
 print "\nSchema keyspaces by query"
 rows = session.execute('SELECT * FROM system_schema.keyspaces')
