4a9034beaac1cbcf53f38ea6d92aaa10a843743f
zanasrin <zanasrin@microsoft.com>
2017-11-13 11:24:58 +0530

    Update Paging.py

--- PythonSamples/Paging.py
+++ PythonSamples/Paging.py
@@ -5,6 +5,8 @@ from cassandra.query import BatchStatement, SimpleStatement
 from prettytable import PrettyTable
 import datetime
 import threading
+from ssl import PROTOCOL_TLSv1_2
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
 
 def PrintTable(rows):
     t = PrettyTable(['City', 'Month', 'Year', 'Usage'])
@@ -33,9 +35,16 @@ class PagedResultHandler(object):
         self.finished_event.set()
     
 
+ssl_opts = {
+    'ca_certs': DEFAULT_CA_BUNDLE_PATH,
+    'ssl_version': PROTOCOL_TLSv1_2,
+}
+if 'selfsigned_cert' in cfg.config:
+    ssl_opts['ca_certs'] = cfg.config['selfsigned_cert']
+
 auth_provider = PlainTextAuthProvider(
         username=cfg.config['username'], password=cfg.config['password'])
-cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_provider=auth_provider)
+cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_provider=auth_provider, ssl_options=ssl_opts)
 session = cluster.connect()
 
 session.execute('CREATE KEYSPACE IF NOT EXISTS Electricity  WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\': \'3\' }');
