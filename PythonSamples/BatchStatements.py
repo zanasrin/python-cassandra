f2332d5c1d20d25e8096c215b7dadc3b48b6b447
zanasrin <zanasrin@microsoft.com>
2017-11-13 11:21:15 +0530

    connect using ssl

--- PythonSamples/BatchStatements.py
+++ PythonSamples/BatchStatements.py
@@ -3,10 +3,19 @@ from cassandra.auth import PlainTextAuthProvider
 import config as cfg
 from cassandra.query import BatchStatement, SimpleStatement
 from prettytable import PrettyTable
+from ssl import PROTOCOL_TLSv1_2
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
+
+ssl_opts = {
+    'ca_certs': DEFAULT_CA_BUNDLE_PATH,
+    'ssl_version': PROTOCOL_TLSv1_2,
+}
+if 'selfsigned_cert' in cfg.config:
+    ssl_opts['ca_certs'] = cfg.config['selfsigned_cert']
 
 auth_provider = PlainTextAuthProvider(
         username=cfg.config['username'], password=cfg.config['password'])
-cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_provider=auth_provider)
+cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_provider=auth_provider, ssl_options=ssl_opts)
 session = cluster.connect()
 
 session.execute('CREATE KEYSPACE IF NOT EXISTS cycling WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\': \'3\' }');
