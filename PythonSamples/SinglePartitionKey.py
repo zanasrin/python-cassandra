9d860b88a22ef4bd073ad94b618b96de1e5a0ef6
zanasrin <zanasrin@microsoft.com>
2017-11-13 11:30:44 +0530

    Update SinglePartitionKey.py

--- PythonSamples/SinglePartitionKey.py
+++ PythonSamples/SinglePartitionKey.py
@@ -3,6 +3,8 @@ from cassandra.auth import PlainTextAuthProvider
 import config as cfg
 from cassandra.query import BatchStatement, SimpleStatement
 from prettytable import PrettyTable
+from ssl import PROTOCOL_TLSv1_2
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
 
 def PrintTable(rows):
     t = PrettyTable(['ID', 'FirstName', 'LastName'])
@@ -10,9 +12,16 @@ def PrintTable(rows):
         t.add_row([user_row.id, user_row.firstname, user_row.lastname])
     print t
 
+ssl_opts = {
+    'ca_certs': DEFAULT_CA_BUNDLE_PATH,
+    'ssl_version': PROTOCOL_TLSv1_2,
+}
+if 'selfsigned_cert' in cfg.config:
+    ssl_opts['ca_certs'] = cfg.config['selfsigned_cert']
+
 auth_provider = PlainTextAuthProvider(
         username=cfg.config['username'], password=cfg.config['password'])
-cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_provider=auth_provider)
+cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_provider=auth_provider, ssl_options=ssl_opts)
 session = cluster.connect()
 
 session.execute('CREATE KEYSPACE IF NOT EXISTS cycling WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\': \'3\' }');
